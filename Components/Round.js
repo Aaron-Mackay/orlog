5cce5399d866980f7b427664569c3c2b1565c8a3
Aaron Mackay <aaronmackay@hotmail.com>
2020-11-30 23:16:21 +0000

    worked out inquirer async/await, implemented dice locking and game text

--- Components/Round.js
+++ Components/Round.js
@@ -1,3 +1,4 @@
+const inquirer = require("inquirer");
 const Dice = require("./Dice")
 const Player = require("./Player")
 
@@ -15,34 +16,78 @@ class Round {
         let currPlayer = this.players[this.currPlayerIndex];
         let currPlayerDice = currPlayer.dices.map(dice => dice.currFace);
         let currPlayerAddGP = currPlayer.dices.filter(dice => dice.goldCurrFace).length;
+        currPlayer.godPoints += currPlayerAddGP;
 
         this.switchTurn();
 
         let otherPlayer = this.players[this.currPlayerIndex];
         let otherPlayerDice = otherPlayer.dices.map(dice => dice.currFace);
         let otherPlayerAddGP = otherPlayer.dices.filter(dice => dice.goldCurrFace).length;
+        otherPlayer.godPoints += otherPlayerAddGP;
 
 
         console.log(`${currPlayer.name} has\n${currPlayerDice}\nand gains ${currPlayerAddGP} god points.\n\n${otherPlayer.name} has\n${otherPlayerDice}\nand gains ${otherPlayerAddGP} god points`);
+
+        // damage calculation
     }
 
-    playMatch() {
-        for (let turn = 1; turn <= 6; turn++) {
-            let currPlayer = this.players[this.currPlayerIndex]
-            console.log(`${currPlayer.name}'s turn to roll`);
+    async playMatch() {
+        for (let player of this.players) {
+            for (let dice of player.dices) {
+                dice.locked = false;
+            }
+        }
 
+        for (let turn = 1; turn <= 6; turn++) {
+            console.log("\nturn", turn);
+            let currPlayer = this.players[this.currPlayerIndex]
             currPlayer.rollDice();
-            if (turn >= 5) { //final turns for players
+
+            const currDice = currPlayer.dices.filter(dice => !dice.locked)
+            const lockedDice = currPlayer.dices.filter(dice => dice.locked);
+
+            const currDiceNames = currDice.map((dice, i) => {
+                return dice.currFace + (dice.goldCurrFace ? " - G" : "")
+            })
+            const lockedDiceNames = lockedDice.map((dice, i) => {
+                return dice.currFace + (dice.goldCurrFace ? " - G" : "")
+            })
+            console.log("lockeddicenames", lockedDiceNames);
+
+            if (turn >= 5) {
+                //final turns for players
                 currPlayer.dices.forEach(dice => dice.locked = true)
             } else {
-                //player locks pieces - inquirer input?
+                await this.playerLockDices(currPlayer.name, currDiceNames, lockedDiceNames)
+                    .then(({ diceToLock }) => {
+                        diceToLock.forEach(diceName => {
+                            // iterate through all unlocked dice, check for matches and lock
+                            for (let dice of currDice) {
+                                const diceToLockIsGold = diceName.includes(" - G");
+                                if (dice.currFace === diceName.replace(" - G", "") && !dice.locked && diceToLockIsGold === dice.goldCurrFace) {
+                                    dice.locked = true;
+                                    break;
+                                }
+                            }
+                        })
+                    })
             }
-            console.log(currPlayer.dices.map(dice => dice.currFace));
 
             this.switchTurn();
         }
         this.resolve();
         this.switchTurn();
+    }
+
+    playerLockDices(name, currDice, lockedDice) {
+        const lockedDiceStr = lockedDice.length > 0 ? `Locked dice: ${lockedDice.join(" . ")}\n` : "";
+        return inquirer
+            .prompt([{
+                type: "checkbox",
+                message: `${lockedDiceStr}${name}, select dice to lock`,
+                name: "diceToLock",
+                choices: currDice
+            }])
     }
 }
 
--- app.js
+++ app.js
@@ -5,9 +5,16 @@ const Player = require("./Components/Player")
 const Round = require("./Components/Round")
 
 const player1 = new Player("bob");
-const player2 = new Player("rob")
+const player2 = new Player("rob");
 
+async function game() {
+    let roundCount = 1;
+    while (player1.health > 0 && player2.health > 0) {
+        console.log(`\n\n=============================\n========== Round ${roundCount++} ==========\n=============================\n`)
+        const round = new Round(player1, player2);
+        await round.playMatch()
+        player1.health--;
+    }
+}
 
-const round = new Round(player1, player2);
-
-round.playMatch()
+game();
